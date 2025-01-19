9b79b1679d89b1c918d96d376af3763ec03fc6ef
Vadym Mamchur <vadym.mamchur.job@gmail.com>
2025-01-19 15:04:42 +0200

    feat: add basic help and exit commands

---
+++ command_help.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "fmt"
+)
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	fmt.Println("help: Displays a help message")
+	fmt.Println("exit: Exit the Pokedex")
+	return nil
+}
+
--- repl.go
+++ repl.go
@@ -1,32 +1,64 @@
 package main
 
 import (
-    "fmt"
-    "bufio"
-    "os"
-    "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
 
 func startRepl() {
-    scanner := bufio.NewScanner(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin)
 
-    for {
-        fmt.Println("Pokedex >")
-        scanner.Scan()
+	for {
+		fmt.Printf("Pokedex >")
+		scanner.Scan()
 
-        words := cleanInput(scanner.Text())
-        if len(words) == 0 {
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+
+        command, exists := getCommands()[commandName]
+        if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
             continue
-        }
-
-        commandName := words[0]
-        fmt.Printf("Your command was: %s\n", commandName)
-    }
+		} else {
+			fmt.Println("Unknown command")
+            continue
+		}
+	}
 }
 
 func cleanInput(text string) []string {
-    output := strings.ToLower(text)
-    words := strings.Fields(output)
-    return words
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
 }
 
